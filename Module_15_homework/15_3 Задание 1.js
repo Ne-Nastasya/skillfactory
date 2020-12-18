301d9f0a0ee7267dd2bed7da6e45e94172ee5ff1
Ne-Nastasya <gringrif@gmail.com>
2020-12-18 22:43:51 +0700

    add homework

---
+++ Module_15_homework/15_3 Задание 1.js
@@ -0,0 +1,28 @@
+<svg width="180" height="120">
+  <rect width="180" height="40" style="fill:white;"/>
+  <rect y="40" width="180" height="40" style="fill:blue;"/>
+  <rect y="80" width="180" height="40" style="fill:red;"/>
+  </svg>
+  
+  <svg width="180" height="120">
+  <rect width="60" height="120" style="fill:blue";/>
+  <rect x="60" width="60" height="120" style="fill:white";/>
+  <rect x="120" width="60" height="120" style="fill:red";/>
+  </svg>
+  
+  <svg width="180" height="120">
+  <rect width="180" height="120"  style="fill:white" stroke="gray" stroke-width="1";/>
+  <rect y="50" width="180" height="30" style="fill:blue";/>
+  <rect x="50" width="30" height="120" style="fill:blue";/>
+  </svg>
+  
+  <svg width="180" height="120">
+  <rect width="180" height="120"  style="fill:red";/>
+  <rect x="75" y="15" width="30" height="90" style="fill:white";/>
+  <rect x="45" y="45" width="90" height="30" style="fill:white";/>
+</svg>
+
+<svg width="180" height="120">
+  <rect width="180" height="120"  style="fill:white" stroke="gray" stroke-width="1";/>
+  <circle cx="90" cy="60" r="45" style="fill:red";/>
+</svg>
